f35bfd694244a4926a7453f88b35519728595a1c
RoshandeepSingh28 <155190705+RoshandeepSingh28@users.noreply.github.com>
2024-08-23 19:51:51 +0530

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,12 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "oplus_co_apdmkd"  // replace MySSID with your WiFi network name
+#define SECRET_PASS "tohm3279"         // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID_WEATHER_STATION 12397  // MathWorks weather station
+
+#define SECRET_CH_ID 2634892  // Test channel for counting
+#define SECRET_WRITE_APIKEY "VLMO5KCS6HZWQ7WI"  // API Key for Test channel
+
+// You can add more channel IDs and API keys if needed
